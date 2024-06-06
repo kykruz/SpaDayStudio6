a7bb7301e7412fd065d5653cc5ae596ff838ad2c
kykruz <k.kruzan@yahoo.com>
2024-06-06 13:45:08 -0500

    chapter 14

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay6;
+
+[Route("/user")]
+public class UserController : Controller
+{
+    [HttpGet("add")]
+    public IActionResult RenderAddUserForm()
+    {
+        return View("Add");
+    }
+
+    
+    
+    [HttpPost("add")]
+    public IActionResult SubmitAddUserForm(User newUser, string verify) 
+    {
+      if(newUser.Password != verify)
+      {
+        ViewBag.Error = "Passwords must match";
+        ViewBag.User = newUser;
+        return View("Add");
+      }
+      
+      ViewBag.UserName = newUser.UserName;
+      return View("Index");
+    }
+}
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace SpaDay6;
+
+public class User
+{
+    public string UserName {get; set;}
+
+    public string Email {get; set;}
+
+    public string Password {get; set;}
+
+    public User() {}
+    public User(string username, string email, string password) : this()
+    {
+        UserName = username;
+        Email = email;
+        Password = password;
+        
+    }
+}
